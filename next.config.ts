44676d0cce213f2d2ad2b47ff8d4178a9a7a6463
ireallywantthatname <rladsilva@students.nsbm.ac.lk>
2025-08-04 09:30:48 +0530

    ci: implement github actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === "production";
+const basePath = process.env.PAGES_BASE_PATH || "";
+
 const nextConfig: NextConfig = {
-	/* config options here */
+	output: "export",
+	basePath: basePath,
+	assetPrefix: basePath,
+	images: {
+		unoptimized: true,
+		remotePatterns: [],
+	},
+	trailingSlash: true,
+	publicRuntimeConfig: {
+		basePath: basePath,
+	},
 };
 
 export default nextConfig;
+
