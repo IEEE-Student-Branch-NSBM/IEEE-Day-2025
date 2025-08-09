cef45ed575df15909fcc5bda4088ba8e87ec696d
sanadee_2004 <sanadeehaputhanthire@gmail.com>
2025-08-09 20:30:44 +0530

    feat: implement gsap for sponsor images

--- src/component/Sponsors.tsx
+++ src/component/Sponsors.tsx
@@ -1,9 +1,18 @@
+"use client"
 import AesturnumLogo from "../../public/sponsors/Aeturnum.webp";
 import NescafeLogo from "../../public/sponsors/nescafe.webp";
 import OrelitLogo from "../../public/sponsors/orelit.webp";
 import VirtusaLogo from "../../public/sponsors/virtusa.webp";
 import NsbmLogo from "../../public/sponsors/nsbm.webp";
 import Image from "next/image";
+import { gsap } from "gsap";
+import { useRef } from 'react';
+import { useGSAP } from '@gsap/react';
+
+gsap.registerPlugin(useGSAP); // register the hook to avoid React version discrepancies 
+
+
+
 
 const sponsors = [
 	{ name: "aeturnum", logo: AesturnumLogo },
@@ -14,10 +23,16 @@ const sponsors = [
 ];
 
 const Sponsors = () => {
+	useGSAP(() => {
+	// gsap code here...
+	gsap.to('.logo', { x: 360 }); // <-- automatically reverted
+},{ }); // <-- scope is for selector text (optional)
+
 	return <div className = "bg-black">
 		{sponsors.map((sponsor, index)=>(
 			<div>
-				<Image src ={sponsor.logo} alt={sponsor.name}/>
+
+				<Image src ={sponsor.logo} alt={sponsor.name} className="logo"/>
 			</div>
 		))}
 	</div>;
