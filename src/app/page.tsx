2ca1dfa23b0e4e9bcf310c6abc8d91b12b8cab90
AchiniSubasinghe <achinimaheshika12@gmail.com>
2025-08-19 13:21:18 +0530

    feat: make memory cards responsive with full-width and max-width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/hero";
+import Memories from "@/components/memories";
 import Sponsors from "@/components/sponsors";
 
 const HomePage = () => {
   return (
     <section>
+      <Memories />
       <Hero />
       <Sponsors />
     </section>
--- src/components/memories.tsx
+++ src/components/memories.tsx
@@ -1,11 +1,19 @@
 function Memories() {
   return (
-    <div>
-      <h1>Memories</h1>
+    <div className="flex flex-col items-center gap-8 p-4">
+      <div className="bg-blue-200 w-full max-w-6xl h-[500px] rounded-2xl flex justify-center items-center">
+        section 1
+      </div>
+
+      <div className="bg-pink-200 w-full max-w-6xl h-[500px] rounded-2xl flex justify-center items-center">
+        section 2
+      </div>
+
+      <div className="bg-purple-200 w-full max-w-6xl h-[500px] rounded-2xl flex justify-center items-center">
+        section 3
+      </div>
     </div>
   );
 }
 
-
 export default Memories;
-
