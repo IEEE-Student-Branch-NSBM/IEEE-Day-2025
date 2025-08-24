2f4224b4f6ee8bead94be19943b328a527aee7c4
ireallywantthatname <rladsilva@students.nsbm.ac.lk>
2025-08-24 13:14:46 +0530

    style: change opacity

--- src/components/memories.tsx
+++ src/components/memories.tsx
@@ -2,7 +2,7 @@ function Memories() {
 
   return (
     <div className="relative z-50 min-h-screen">
-      <div className="sticky top-1/2 bottom-1/2 -translate-y-1/2 h-50 bg-white/20 backdrop-blur-lg">
+      <div className="sticky top-1/2 bottom-1/2 -translate-y-1/2 h-50 bg-white/5 backdrop-blur-lg">
         <div></div>
       </div>
       {/* <div className="sticky top-1/2 bottom-1/2 -translate-y-1/2 h-screen bg-white/20 backdrop-blur-lg">
--- src/components/sponsors.tsx
+++ src/components/sponsors.tsx
@@ -39,7 +39,7 @@ const Sponsors = () => {
   }, []);
 
   return (
-    <div className="bg-white/20 backdrop-blur-lg flex flex-wrap justify-center items-center gap-6 p-6 z-50 relative">
+    <div className="bg-white/5 backdrop-blur-lg flex flex-wrap justify-center items-center gap-6 p-6 z-50 relative">
       {sponsors.map((sponsor, index) => (
         <div
           key={sponsor.name}
