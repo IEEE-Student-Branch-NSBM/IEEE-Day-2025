57c7d05558fbee8f0a353e63f94a180f4ac6ef68
Tharindu Nimesh <154634446+tharinduxnimesh@users.noreply.github.com>
2025-09-23 15:32:30 +0530

    Update: improved footer layout and content

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,30 +1,176 @@
+'use client';
+
 import Image from "next/image";
-import SbLogo from "../../public/logos/ieee-nsbm-sb-logo.png"
-import CsLogo from "../../public/logos/ieee-nsbm-cs-logo.png"
-import WieLogo from "../../public/logos/ieee-nsbm-wie-logo.png"
+import Link from "next/link";
+import SbLogo from "../../public/logos/ieee-nsbm-sb-logo.png";
+import CsLogo from "../../public/logos/ieee-nsbm-cs-logo.png";
+import WieLogo from "../../public/logos/ieee-nsbm-wie-logo.png";
+import IeeeDayLogo from "../../public/logos/ieee-day-logo-2025.png";
 
 const Footer = () => {
 
-    const logos = [SbLogo, CsLogo, WieLogo];
+    const chapterLogos = [SbLogo, CsLogo, WieLogo];
+
+    const socialLinks = [
+        {
+            name: "Facebook",
+            url: "https://www.facebook.com/ieeensbm/",
+            icon: (
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M15.4024 21V14.0344H17.7347L18.0838 11.3265H15.4024V9.59765C15.4024 8.81364 15.62 8.27934 16.7443 8.27934L18.1783 8.27867V5.85676C17.9302 5.82382 17.0791 5.75006 16.0888 5.75006C14.0213 5.75006 12.606 7.01198 12.606 9.32952V11.3265H10.2677V14.0344H12.606V21H4C3.44772 21 3 20.5523 3 20V4C3 3.44772 3.44772 3 4 3H20C20.5523 3 21 3.44772 21 4V20C21 20.5523 20.5523 21 20 21H15.4024Z"></path></svg>
+            )
+        },
+        {
+            name: "Instagram",
+            url: "https://www.instagram.com/ieee_nsbm/",
+            icon: (
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M13.0281 2.00073C14.1535 2.00259 14.7238 2.00855 15.2166 2.02322L15.4107 2.02956C15.6349 2.03753 15.8561 2.04753 16.1228 2.06003C17.1869 2.1092 17.9128 2.27753 18.5503 2.52503C19.2094 2.7792 19.7661 3.12253 20.3219 3.67837C20.8769 4.2342 21.2203 4.79253 21.4753 5.45003C21.7219 6.0867 21.8903 6.81337 21.9403 7.87753C21.9522 8.1442 21.9618 8.3654 21.9697 8.58964L21.976 8.78373C21.9906 9.27647 21.9973 9.84686 21.9994 10.9723L22.0002 11.7179C22.0003 11.809 22.0003 11.903 22.0003 12L22.0002 12.2821L21.9996 13.0278C21.9977 14.1532 21.9918 14.7236 21.9771 15.2163L21.9707 15.4104C21.9628 15.6347 21.9528 15.8559 21.9403 16.1225C21.8911 17.1867 21.7219 17.9125 21.4753 18.55C21.2211 19.2092 20.8769 19.7659 20.3219 20.3217C19.7661 20.8767 19.2069 21.22 18.5503 21.475C17.9128 21.7217 17.1869 21.89 16.1228 21.94C15.8561 21.9519 15.6349 21.9616 15.4107 21.9694L15.2166 21.9757C14.7238 21.9904 14.1535 21.997 13.0281 21.9992L12.2824 22C12.1913 22 12.0973 22 12.0003 22L11.7182 22L10.9725 21.9993C9.8471 21.9975 9.27672 21.9915 8.78397 21.9768L8.58989 21.9705C8.36564 21.9625 8.14444 21.9525 7.87778 21.94C6.81361 21.8909 6.08861 21.7217 5.45028 21.475C4.79194 21.2209 4.23444 20.8767 3.67861 20.3217C3.12278 19.7659 2.78028 19.2067 2.52528 18.55C2.27778 17.9125 2.11028 17.1867 2.06028 16.1225C2.0484 15.8559 2.03871 15.6347 2.03086 15.4104L2.02457 15.2163C2.00994 14.7236 2.00327 14.1532 2.00111 13.0278L2.00098 10.9723C2.00284 9.84686 2.00879 9.27647 2.02346 8.78373L2.02981 8.58964C2.03778 8.3654 2.04778 8.1442 2.06028 7.87753C2.10944 6.81253 2.27778 6.08753 2.52528 5.45003C2.77944 4.7917 3.12278 4.2342 3.67861 3.67837C4.23444 3.12253 4.79278 2.78003 5.45028 2.52503C6.08778 2.27753 6.81278 2.11003 7.87778 2.06003C8.14444 2.04816 8.36564 2.03847 8.58989 2.03062L8.78397 2.02433C9.27672 2.00969 9.8471 2.00302 10.9725 2.00086L13.0281 2.00073ZM12.0003 7.00003C9.23738 7.00003 7.00028 9.23956 7.00028 12C7.00028 14.7629 9.23981 17 12.0003 17C14.7632 17 17.0003 14.7605 17.0003 12C17.0003 9.23713 14.7607 7.00003 12.0003 7.00003ZM12.0003 9.00003C13.6572 9.00003 15.0003 10.3427 15.0003 12C15.0003 13.6569 13.6576 15 12.0003 15C10.3434 15 9.00028 13.6574 9.00028 12C9.00028 10.3431 10.3429 9.00003 12.0003 9.00003ZM17.2503 5.50003C16.561 5.50003 16.0003 6.05994 16.0003 6.74918C16.0003 7.43843 16.5602 7.9992 17.2503 7.9992C17.9395 7.9992 18.5003 7.4393 18.5003 6.74918C18.5003 6.05994 17.9386 5.49917 17.2503 5.50003Z"></path></svg>
+            )
+        },
+        {
+            name: "LinkedIn",
+            url: "https://lk.linkedin.com/company/ieeesbnsbm",
+            icon: (
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M18.3362 18.339H15.6707V14.1622C15.6707 13.1662 15.6505 11.8845 14.2817 11.8845C12.892 11.8845 12.6797 12.9683 12.6797 14.0887V18.339H10.0142V9.75H12.5747V10.9207H12.6092C12.967 10.2457 13.837 9.53325 15.1367 9.53325C17.8375 9.53325 18.337 11.3108 18.337 13.6245V18.339H18.3362ZM7.00373 8.57475C6.14573 8.57475 5.45648 7.88025 5.45648 7.026C5.45648 6.1725 6.14648 5.47875 7.00373 5.47875C7.85873 5.47875 8.55173 6.1725 8.55173 7.026C8.55173 7.88025 7.85798 8.57475 7.00373 8.57475ZM8.34023 18.339H5.66723V9.75H8.34023V18.339ZM19.6697 3H4.32923C3.59498 3 3.00098 3.5805 3.00098 4.29675V19.7033C3.00098 20.4202 3.59498 21 4.32923 21H19.6675C20.401 21 21.001 20.4202 21.001 19.7033V4.29675C21.001 3.5805 20.401 3 19.6675 3H19.6697Z"></path></svg>
+            )
+        },
+        {
+            name: "Twitter",
+            url: "https://x.com/nsbmieee",
+            icon: (
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M17.6874 3.0625L12.6907 8.77425L8.37045 3.0625H2.11328L9.58961 12.8387L2.50378 20.9375H5.53795L11.0068 14.6886L15.7863 20.9375H21.8885L14.095 10.6342L20.7198 3.0625H17.6874ZM16.6232 19.1225L5.65436 4.78217H7.45745L18.3034 19.1225H16.6232Z"></path></svg>
+            )
+        },
+        {
+            name: "YouTube",
+            url: "https://www.youtube.com/@ieeensbm",
+            icon: (
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M12.2439 4C12.778 4.00294 14.1143 4.01586 15.5341 4.07273L16.0375 4.09468C17.467 4.16236 18.8953 4.27798 19.6037 4.4755C20.5486 4.74095 21.2913 5.5155 21.5423 6.49732C21.942 8.05641 21.992 11.0994 21.9982 11.8358L21.9991 11.9884L21.9991 11.9991C21.9991 11.9991 21.9991 12.0028 21.9991 12.0099L21.9982 12.1625C21.992 12.8989 21.942 15.9419 21.5423 17.501C21.2878 18.4864 20.5451 19.261 19.6037 19.5228C18.8953 19.7203 17.467 19.8359 16.0375 19.9036L15.5341 19.9255C14.1143 19.9824 12.778 19.9953 12.2439 19.9983L12.0095 19.9991L11.9991 19.9991C11.9991 19.9991 11.9956 19.9991 11.9887 19.9991L11.7545 19.9983C10.6241 19.9921 5.89772 19.941 4.39451 19.5228C3.4496 19.2573 2.70692 18.4828 2.45587 17.501C2.0562 15.9419 2.00624 12.8989 2 12.1625V11.8358C2.00624 11.0994 2.0562 8.05641 2.45587 6.49732C2.7104 5.51186 3.45308 4.73732 4.39451 4.4755C5.89772 4.05723 10.6241 4.00622 11.7545 4H12.2439ZM9.99911 8.49914V15.4991L15.9991 11.9991L9.99911 8.49914Z"></path></svg>
+            )
+        },
+        {
+            name: "GitHub",
+            url: "https://github.com/IEEE-Student-Branch-NSBM/",
+            icon: (
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M12.001 2C6.47598 2 2.00098 6.475 2.00098 12C2.00098 16.425 4.86348 20.1625 8.83848 21.4875C9.33848 21.575 9.52598 21.275 9.52598 21.0125C9.52598 20.775 9.51348 19.9875 9.51348 19.15C7.00098 19.6125 6.35098 18.5375 6.15098 17.975C6.03848 17.6875 5.55098 16.8 5.12598 16.5625C4.77598 16.375 4.27598 15.9125 5.11348 15.9C5.90098 15.8875 6.46348 16.625 6.65098 16.925C7.55098 18.4375 8.98848 18.0125 9.56348 17.75C9.65098 17.1 9.91348 16.6625 10.201 16.4125C7.97598 16.1625 5.65098 15.3 5.65098 11.475C5.65098 10.3875 6.03848 9.4875 6.67598 8.7875C6.57598 8.5375 6.22598 7.5125 6.77598 6.1375C6.77598 6.1375 7.61348 5.875 9.52598 7.1625C10.326 6.9375 11.176 6.825 12.026 6.825C12.876 6.825 13.726 6.9375 14.526 7.1625C16.4385 5.8625 17.276 6.1375 17.276 6.1375C17.826 7.5125 17.476 8.5375 17.376 8.7875C18.0135 9.4875 18.401 10.375 18.401 11.475C18.401 15.3125 16.0635 16.1625 13.8385 16.4125C14.201 16.725 14.5135 17.325 14.5135 18.2625C14.5135 19.6 14.501 20.675 14.501 21.0125C14.501 21.275 14.6885 21.5875 15.1885 21.4875C19.259 20.1133 21.9999 16.2963 22.001 12C22.001 6.475 17.526 2 12.001 2Z"></path></svg>
+            )
+        }
+    ];
+
+    const contactInfo = [
+        {
+            label: "Email",
+            value: "nsbmieee@gmail.com",
+            href: "mailto:nsbmieee@gmail.com",
+            isLink: true,
+            icon: (
+                <svg className="fill-white h-4 md:h-8 w-4 md:w-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M3 3H21C21.5523 3 22 3.44772 22 4V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4C2 3.44772 2.44772 3 3 3ZM12.0606 11.6829L5.64722 6.2377L4.35278 7.7623L12.0731 14.3171L19.6544 7.75616L18.3456 6.24384L12.0606 11.6829Z"></path></svg>
+            )
+        },
+        {
+            label: "Website",
+            value: "www.ieeensbm.org",
+            href: "https://www.ieeensbm.org/",
+            isLink: true,
+            icon: (
+                <svg className="fill-white h-4 md:h-8 w-4 md:w-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M20 22H4C3.44772 22 3 21.5523 3 21V8H21V21C21 21.5523 20.5523 22 20 22ZM21 6H3V3C3 2.44772 3.44772 2 4 2H20C20.5523 2 21 2.44772 21 3V6ZM7 11V15H11V11H7ZM7 17V19H17V17H7ZM13 12V14H17V12H13Z"></path></svg>
+            )
+        },
+        {
+            label: "Location",
+            value: "NSBM Green University, Homagama, Sri Lanka",
+            href: "https://maps.google.com/?q=NSBM+Green+University+Homagama+Sri+Lanka",
+            isLink: true,
+            icon: (
+                <svg className="fill-white h-4 md:h-8 w-4 md:w-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M18.364 17.364L12 23.7279L5.63604 17.364C2.12132 13.8492 2.12132 8.15076 5.63604 4.63604C9.15076 1.12132 14.8492 1.12132 18.364 4.63604C21.8787 8.15076 21.8787 13.8492 18.364 17.364ZM12 15C14.2091 15 16 13.2091 16 11C16 8.79086 14.2091 7 12 7C9.79086 7 8 8.79086 8 11C8 13.2091 9.79086 15 12 15ZM12 13C10.8954 13 10 12.1046 10 11C10 9.89543 10.8954 9 12 9C13.1046 9 14 9.89543 14 11C14 12.1046 13.1046 13 12 13Z"></path></svg>
+            )
+        }
+    ];
 
     return (
-        <div className="relative z-50 w-full h-auto min-h-32 bg-black overflow-hidden py-6">
-            <div className="container mx-auto px-4">
-                <div className="grid grid-cols-3 items-center justify-items-center h-full gap-6 sm:gap-6 md:gap-12">
-                    {logos.map((logo, index) => (
-                        <div key={index} className="flex items-center justify-center w-full h-16 sm:h-16 md:h-24">
-                            <Image
-                                src={logo}
-                                alt=""
-                                width={200}
-                                height={200}
-                                className="object-contain max-w-full max-h-full w-auto h-auto scale-90 sm:scale-90 md:scale-100"
-                            />
+        <footer className="relative z-50 w-full h-auto bg-black text-white backdrop-blur-lg overflow-hidden py-2">
+            <div className="container mx-auto">
+                <div className="p-6 md:p-10">
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-14 md:gap-10 mb-10">
+                        <div className="flex flex-col items-center justify-center space-y-4">
+                            <div className="flex items-center mb-2">
+                                <Image
+                                    src={IeeeDayLogo}
+                                    alt="IEEE Day 2025"
+                                    width={200}
+                                    height={200}
+                                    className="object-contain scale-60 md:scale-100 w-auto"
+                                />
+                            </div>
+                            <div className="text-white/80 text-sm md:text-base leading-relaxed text-center max-w-md">
+                                IEEE Day celebrates the first time in history when engineers worldwide gathered to share their technical ideas in 1884. Join us for IEEE Day 2025 at NSBM Green University.
+                            </div>
+
+                            <div className="flex flex-wrap gap-2">
+                                {socialLinks.map((social, index) => (
+                                    <a
+                                        key={index}
+                                        href={social.url}
+                                        className="w-8 h-8 flex items-center justify-center hover:scale-110 transition-transform duration-500"
+                                    >
+                                        {social.icon}
+                                    </a>
+                                ))}
+                            </div>
                         </div>
-                    ))}
+
+                        <div className="flex flex-col items-center justify-center space-y-4">
+                            <div className="space-y-3">
+                                {contactInfo.map((contact, index) => (
+                                    <div key={index} className="flex items-start">
+                                        <div className="mr-3 mt-0.5 p-1.5 flex items-center justify-center">
+                                            {contact.icon}
+                                        </div>
+                                        <div>
+                                            <div className="text-white/80 text-xs md:text-sm">{contact.label}</div>
+                                            {contact.isLink ? (
+                                                <a
+                                                    href={contact.href}
+                                                    className="text-sm md:text-base flex items-center"
+                                                >
+                                                    {contact.value}
+                                                    <svg className="w-3 h-3 ml-1 opacity-70" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                                        <path d="M19 19H5V5h7V3H5a2 2 0 00-2 2v14a2 2 0 002 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z" />
+                                                    </svg>
+                                                </a>
+                                            ) : (
+                                                <div className="text-sm">{contact.value}</div>
+                                            )}
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="pt-8 border-t border-white/10">
+                        <div className="grid grid-cols-1 sm:grid-cols-3 items-center justify-items-center h-full">
+                            {chapterLogos.map((logo, index) => (
+                                <div key={index}>
+                                    <Image
+                                        src={logo}
+                                        alt=""
+                                        width={400}
+                                        height={400}
+                                        className="object-cover scale-50 md:scale-75"
+                                    />
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+
+                <div className="text-center">
+                    <div className="text-white/60 text-xs md:text-sm">
+                        © IEEE NSBM Student Branch. All rights reserved.
+                    </div>
                 </div>
             </div>
-        </div>
+        </footer>
     )
 }
 
